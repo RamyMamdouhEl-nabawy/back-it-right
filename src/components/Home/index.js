29ac01324dad550e15aa3de8899fc134d49b7274
MMostafa-itworx <mohamed.mostafa@itworx.com>
2021-07-04 14:57:19 +0200

    first

---
+++ src/components/Home/index.js
@@ -0,0 +1,173 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProjectsList from "../reusableComponents/ProjectsList";
+import { Button } from "react-bootstrap";
+import Switch from "react-switch";
+
+import "./styles.scss";
+import AuthProvider from "../AuthProvider";
+
+const Home = (props) => {
+  const baseUrl = "http://backitright.pythonanywhere.com";
+  const token = localStorage.getItem("token");
+  const [checked, setChecked] = useState(false);
+  const [logMessages, setLogMessages] = useState([]);
+  const [organizations, setOrganizations] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [planData, setPlanData] = useState([]);
+  const [backups, setBackups] = useState([]);
+
+  const handleToggle = () => {
+    setChecked(!checked);
+  };
+
+  const handleLogs = async () => {
+    const response = await axios({
+      method: "get",
+      url: `${baseUrl}/logs/`,
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    setLogMessages(response.data);
+  };
+
+  const getRepoBackups = async () => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: `${baseUrl}/repository/backups/`,
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setBackups(response.data);
+    } catch (ex) {
+      if (ex.response.status === 401) {
+        AuthProvider.getAccessToken();
+      }
+    }
+  };
+
+  const getRepos = async (Org) => {
+    const orgName = Org.name;
+    const orgVal = Org.value;
+    try {
+      const response = await axios({
+        method: "get",
+        url: `${baseUrl}/repositories/?organization=${orgVal}`,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setProjects(response.data);
+      setPlanData((prevState) => {
+        return {
+          ...prevState,
+          [orgName]: orgVal
+        };
+      });
+      getRepoBackups();
+    } catch (ex) {
+      if (ex.response.status === 401) {
+        AuthProvider.getAccessToken();
+      }
+    }
+  };
+
+  const fetchOrg = async () => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: `${baseUrl}/organizations/`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+      setOrganizations(response.data);
+    } catch (ex) {
+      if (ex.response.status === 401) {
+        AuthProvider.getAccessToken();
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchOrg();
+  }, []);
+
+  const handleTimeJob = (jobDate) => {
+    const jobName = jobDate.name;
+    const jobVal = jobDate.value;
+    setPlanData((prevState) => {
+      return {
+        ...prevState,
+        [jobName]: jobVal
+      };
+    });
+    // setJob(jobData);
+    // console.log(jobData);
+  };
+
+  const handleNewRepoPath = (path) => {
+    const pathName = path.name;
+    const pathVal = path.value;
+    setPlanData((prevState) => {
+      return {
+        ...prevState,
+        [pathName]: pathVal
+      };
+    });
+  };
+
+  return (
+    <>
+      <section className="row back-right">
+        <div className="col-3">
+          <Button className="col-12 col-md-8 btn btn-secondary back-right__btn">BackUp</Button>
+          <Button className="col-12 col-md-8 btn btn-primary back-right__btn">Save</Button>
+          <div className="row justify-content-center my-3">
+            <div className="col-12 col-md-7">Make Trigger Job</div>
+            <div className="col-12 col-md-3">
+              <Switch onChange={handleToggle} checked={checked} />
+            </div>
+          </div>
+        </div>
+        <div className="col-9 shadow-sm border border-2 back-right__right-side">
+          <section className="m-2">
+            <ProjectsList
+              trigger={checked}
+              accessToken={token}
+              organizations={organizations}
+              projects={projects}
+              planData={planData}
+              backups={backups}
+              onRepoChange={(e) => getRepos(e.target)}
+              onTimeJobChange={(e) => handleTimeJob(e.target)}
+              onRepoNameChange={(e) => handleNewRepoPath(e.target)}
+            />
+
+            <div className="input-group my-2">
+              <span className="input-group-text">Log Area</span>
+              <textarea
+                className="form-control"
+                aria-label="With textarea"
+                rows={4}
+                defaultValue={logMessages.map((msg) => msg.message)}
+              ></textarea>
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Login/index.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import AuthProvider from "../AuthProvider";
+import { toast } from "react-toastify";
+import Form from "react-bootstrap/Form";
+import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./styles.scss";
+
+const Login = (props) => {
+  const { route } = props;
+  const [formData, setFormData] = useState({});
+
+  const setFormValues = (targetElem) => {
+    const Elem = targetElem;
+    const name = Elem.name;
+    const value = Elem.type === "checkbox" ? Elem.checked : Elem.value;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const CustomToastMsg = (Error) => {
+    return <div>{Error}</div>;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await AuthProvider.authenticateUser(formData);
+
+      route.history.push("/home");
+    } catch (ex) {
+      if (ex.response && ex.response.status === 401) {
+        const msg = ex.response.data.detail;
+        toast.error(CustomToastMsg(msg), {
+          position: "top-right",
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="row">
+      <div className="col-7 align-self-center">
+        <img
+          className=" back-right__login-logo"
+          src="images/Back-it-right-logo.jpg"
+          alt="Back-It-Right-Logo"
+        />
+      </div>
+      <div className="col-5 align-self-center">
+        <div className="card shadow-sm">
+          <div className="card-body">
+            <Form onSubmit={handleSubmit}>
+              <Form.Row className="align-items-center">
+                <Col xs="auto text-start">
+                  <Form.Label className="col-2" htmlFor="fN">
+                    userName
+                  </Form.Label>
+                  <Form.Control
+                    id="fN"
+                    required
+                    className="d-inline w-75 m-3"
+                    name="username"
+                    placeholder="Jone Doe"
+                    onChange={(e) => setFormValues(e.target)}
+                  />
+                </Col>
+                <Col xs="auto text-start">
+                  <Form.Label className="col-2" htmlFor="lN">
+                    Password
+                  </Form.Label>
+                  <Form.Control
+                    id="lN"
+                    required
+                    type="password"
+                    placeholder="*******"
+                    className="d-inline w-75 m-3"
+                    name="password"
+                    onChange={(e) => setFormValues(e.target)}
+                  />
+                </Col>
+
+                <Col xs="auto">
+                  <Button
+                    type="submit"
+                    className="btn btn-primary back-right__form-btn mx-3"
+                  >
+                    Log In
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
